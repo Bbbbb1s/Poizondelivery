9e0d840023ac90f68f0e85e422a405777841e597
Bbbbb1s <john.may.1990@mail.ru>
2024-05-26 04:26:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -47,9 +47,5 @@ document.addEventListener('DOMContentLoaded', function () {
     shoesButton.addEventListener('click', function () {
         selectButton(this);
     });
-
-    // Прокрутка при фокусе на инпуте
-    input1.addEventListener('focus', function () {
-        document.querySelector('.calculator').scrollIntoView({ behavior: 'smooth' });
-    });
 });
+
